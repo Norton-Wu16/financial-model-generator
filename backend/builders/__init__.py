10e07ac294fcadb59c3c5dbdb9046ed5fd1cc66a
Norton-Wu16 <wunuocheng@126.com>
2026-09-17 16:26:07 +0800

    Delete backend directory

--- backend/builders/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Excel 构建器包。"""
-from backend.builders.three_statement import build as build_three_statement
-from backend.builders.dcf import build as build_dcf
-from backend.builders.lbo import build as build_lbo
